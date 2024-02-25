cdedfba15bffe944815e163c0363b19a01dcaf06
Martim Redondo <116294262+100664@users.noreply.github.com>
2024-02-25 16:33:27 +0000

    Update tp3.py

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -1,29 +1,30 @@
 import re
 
-filename = "input.txt" 
-output_file = "output.txt"
+filename = "input.txt"  
+output_file = "output.txt" 
 
 with open(filename, 'r') as file:
     stdin = file.read()
 
 print("O arquivo foi lido com sucesso.")
 
-regex = r'\bon\b|\d+|off|='
-text = re.findall(regex, input, flags=re.IGNORECASE)
+symbols_regex = r'\bon\b|\d+|off|='
+symbols = re.findall(symbols_regex, stdin, flags=re.IGNORECASE)
 
 state = False 
-soma = 0
+current = 0
 
-output_lines = [] 
-for char in text:
-    if char.lower() == 'on':
+output_lines = []
+
+for symbol in symbols:
+    if symbol.lower() == 'on':
         state = True
-    elif char.lower() == 'off':
+    elif symbol.lower() == 'off':
         state = False
-    elif char.isdigit() and state:
-        soma += int(char)
-    elif char == '=':
-        output_lines.append(f'Soma: {soma}') 
+    elif symbol.isdigit() and state:
+        current += int(symbol)
+    elif symbol == '=':
+        output_lines.append(f'Soma: {current}')
 
 with open(output_file, 'w') as output:
     for line in output_lines:
